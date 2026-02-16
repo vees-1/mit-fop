ed63978aef8d4065a679ca873bf5de2a9fc50b4b
Vineet <vineet@Vineets-MacBook-Pro.local>
2026-02-16 23:35:42 +0530

    modified

--- assn_11.c
+++ assn_11.c
@@ -41,12 +41,18 @@ int main(void)
 bool is_prime(int n)
 {
     if (n <= 1)
+    {
         return false;
+    }
 
     for (int i = 2; i * i <= n; i++)
+    {
         if (n % i == 0)
+        {
             return false;
-
+        }
+    }
+    
     return true;
 }
 
