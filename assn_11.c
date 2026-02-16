ae4129f9c11c33a42ccbc41a8deac2e22ba9905c
Vineet <vineet@Vineets-MacBook-Pro.local>
2026-02-16 23:40:57 +0530

    modified

--- assn_11.c
+++ assn_11.c
@@ -15,24 +15,36 @@ int main(void)
     printf("Cube = %d\n", number * number * number);
 
     if (number >= 0)
+    {
         printf("Square Root = %.2f\n", sqrt(number));
+    }
     else
+    {
         printf("Square Root not defined for negative numbers.\n");
+    }
 
     if (is_prime(number))
+    {
         printf("It is a Prime number.\n");
+    }
     else
+    {
         printf("It is NOT a Prime number.\n");
+    }
 
     if (number >= 0)
     {
         long long fact = 1;
         for (int i = 1; i <= number; i++)
+        {
             fact *= i;
+        }
         printf("Factorial = %lld\n", fact);
     }
     else
+    {
         printf("Factorial not defined for negative numbers.\n");
+    }
 
     printf("Prime Factors: ");
     prime_factors(number);
